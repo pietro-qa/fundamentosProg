6baf41ada2818a22cf5f1b1873e71f1f51ece3fd
pietro-qa <pietroassis00@outlook.com>
2021-06-28 12:32:49 -0300

    desafio 2

--- desfaio.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main ()
-{
-    printf("Valor de n = %d", 2*2*2);
-}
---
+++ imposto.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+    int main (){
+        float value, impostoTotal;
+        scanf("%f", &value);
+
+            if(value > 4000){
+                impostoTotal = (value - 4000) * 0.275;
+                value = 4000;
+            }else;
+            if (value <= 4000 && value >= 3500.76){
+                impostoTotal = ((value - 3500.75) * 0.225 )+ impostoTotal;
+                value = 3500.75; 
+            }else;
+            if (value <= 3500.75 && value >= 2300.51){
+                impostoTotal = ((value - 2300.50) * 0.15 )+ impostoTotal; 
+                value = 2300.50;
+            }else;
+            if (value <= 2300.50 && value >= 1200.26){
+                impostoTotal = ((value - 1200.25) * 0.075 )+ impostoTotal; 
+                printf("%.2f", impostoTotal);
+            }else printf("0");
+        
+    }
